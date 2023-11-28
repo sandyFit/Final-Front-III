12005e907e4fbd87d8b0cef46786babed6ce62ac
SandyFit <sandy07r@gmail.com>
2023-11-28 15:58:29 -0500

    Fixing inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+import { showAlert } from '../utils/showAlert';
+import '../assets/form.css';
 
-import React from 'react'
-  // Hooks
-import { useState } from 'react'
-import { showAlert } from '../utils/showAlert'
-import '../assets/form.css'
-
-
-// Component
 const Form = () => {
-
-  const [state_1, setState_1] = useState("");
-  const [state_2, setState_2] = useState("");
-
-  const [stateMessage, setMessage] = useState("");
+  const [state_1, setState_1] = useState('');
+  const [state_2, setState_2] = useState('');
+  const [stateMessage, setMessage] = useState('');
 
   const handleChange_1 = (e) => {
     setState_1(e.target.value);
@@ -22,58 +16,69 @@ const Form = () => {
     setState_2(e.target.value);
   };
 
-
   const handleSubmit = (e) => {
+    e.preventDefault();
 
-    e.preventDefault()
+    let nameValidation = /^.{5,}$/.test(state_1);
+    let emailValidation = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+      state_2
+    );
 
-    let nameValidation = /^.{5,}$/.test(state_1)
-  
-    let emailValidation = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(state_2)
-
-    if (nameValidation && emailValidation) {
-      showAlert(`Thank you ${state_1}, We will be contacting you by mail: ${state_2}`)
-      console.log("Succesful");
-    } else {
-      showAlert("Please check your information again")
-      console.error("Unsuccesful");
+    if (nameValidation && emailValidation)
+    {
+      showAlert(
+        `Thank you ${state_1}, We will be contacting you by mail: ${state_2}`,
+        'success'
+      );
+      console.log('Successful');
+    } else
+    {
+      showAlert('Please check your information again', 'warning');
+      console.error('Unsuccessful');
     }
-  }
+
+    // Limpiar los campos después de enviar el formulario
+    setState_1('');
+    setState_2('');
+  };
 
   return (
-    <div className = 'Form__div'>
-      <form className = 'div__form'>
+    <div className='Form__div'>
+      <form className='div__form'>
         <div>
-          <input onChange={handleChange_1}
-            className={`${state_1 ? "has-value" : ""}`}
-            id="name"
-            type="text"
+          <input
+            onChange={handleChange_1}
+            className={`${state_1 ? 'has-value' : ''}`}
+            id='name'
+            type='text'
             value={state_1}
-            placeholder="Your Name"
+            placeholder='Your Name'
             required
           />
         </div>
 
         <div>
-          <input onChange={handleChange_2}
-            className={`${state_2 ? "has-value" : ""}`}
-            id="email"
-            type="email"
-            placeholder="Your Email"
+          <input
+            onChange={handleChange_2}
+            className={`${state_2 ? 'has-value' : ''}`}
+            id='email'
+            type='email'
+            value={state_2}
+            placeholder='Your Email'
             required
           />
         </div>
-        
-        <button onClick = {handleSubmit} typeof = 'submit'>
-          <div className = 'button__div' required>
+
+        <button onClick={handleSubmit} type='submit'>
+          <div className='button__div' required>
             Send
           </div>
         </button>
 
         <p>{stateMessage}</p>
       </form>
-
     </div>
-  )
-}
-export {Form};
+  );
+};
+
+export { Form };
